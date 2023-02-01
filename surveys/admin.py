01460ab5f8a9fd9f9b2629bdf53fb4412fa3ded8
inmass <inmass.idbel@gmail.com>
2023-02-01 22:49:17 +0100

    organize surveys models

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Survey)
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Response)
--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.utils.text import slugify
+from django.conf import settings
 
 # Create your models here.
+
+class Survey(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            slug = slugify(self.title)
+            has_slug = Survey.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.title) + '-' + str(count)
+                has_slug = Survey.objects.filter(slug=slug).exists()
+
+            self.slug = slug
+        
+        super().save(*args, **kwargs)
+
+QESTIONS_TYPES = (
+    ('text', 'Text'),
+    ('number', 'Number'),
+    ('date', 'Date'),
+    ('radio', 'Radio'),
+    ('checkbox', 'Checkbox'),
+    ('select', 'Select'),
+)
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    type = models.CharField(max_length=8, choices=QESTIONS_TYPES)
+    text = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.choice_text
+
+class SurveyResponse(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.survey.title
+
+class Response(models.Model):
+    survey_response = models.ForeignKey(SurveyResponse, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.choice.choice_text
